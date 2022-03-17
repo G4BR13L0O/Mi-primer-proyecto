4f50a1a202dbcc9a3974c7b7af0873a895700e67
G4BR13L0O <101760862+G4BR13L0O@users.noreply.github.com>
2022-03-16 19:07:02 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,5 @@
+console.log("hola");
+console.log("Bienvenidos a Visual Studio Code");
+console.log("No se que escribir XD");
+console.log("Como estan??")
+console.log("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
